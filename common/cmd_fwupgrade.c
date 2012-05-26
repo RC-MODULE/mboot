8de0d29fa7d452f3e084bcef7c4cd04b62e9d97d
Sergey Mironov <ierton@gmail.com>
2012-05-26 23:21:37 +0400

    fwupgrade: complete the implementation

--- common/cmd_fwupgrade.c
+++ common/cmd_fwupgrade.c
@@ -6,30 +6,93 @@
 struct fwu_tftp_ctx {
 	struct mtd_info *mtd;
 	u8* page;
+	size_t page_sz;
 	loff_t flash_offset;
 	loff_t flash_end;
+	int bb;
 };
 
-static struct fwu_tftp_ctx fwu_ctx;
-static int fwu_tftp_cb(uint32_t off, u8* buf, size_t len, void* priv)
+static int fwu_tftp_cb(uint32_t off, u8* buf, size_t len, int last, void* priv)
 {
-	struct fwu_tftp_ctx * ctx = (struct fwu_tftp_ctx*)priv;
+	int ret;
+	struct fwu_tftp_ctx *ctx = (struct fwu_tftp_ctx*)priv;
+
 	if(ctx == NULL) {
 		return -EINVAL;
 	}
 
-	printf("FWU got data: off 0x%08X len %d\n", off, len);
-	return 0;
+	struct mtd_info *mtd = ctx->mtd;
+
+	ret = 0;
+	while(len > 0) {
+		size_t len1 = MIN(mtd->writesize - ctx->page_sz, len);
+		memcpy(&ctx->page[ctx->page_sz], buf, len1);
+
+		ctx->page_sz += len1;
+		buf += len1;
+		len -= len1;
+
+		if((ctx->page_sz == mtd->writesize) || last) {
+			if(ctx->flash_offset % mtd->erasesize == 0) {
+				ret = mtd_block_isbad(mtd, ctx->flash_offset);
+				if(ret < 0) {
+					printf("FWU bad block detection failure: off 0x%012llX\n",
+						ctx->flash_offset);
+					goto err;
+				}
+				else if (ret == 1) {
+					printf("FWU skipping bad block: off 0x%012llX\n",
+						ctx->flash_offset);
+					ctx->flash_offset += mtd->erasesize;
+					ctx->bb ++;
+					continue;
+				}
+
+				printf("FWU rewriting block: off 0x%012llX\n",
+					ctx->flash_offset);
+
+				ret = mtd_erase1(mtd, ctx->flash_offset);
+				if(ret < 0) {
+					printf("FWU erase err: off 0x%012llX ret %d\n",
+						ctx->flash_offset, ret);
+					goto err;
+				}
+			}
+
+			ret = mtd_write(mtd, ctx->flash_offset, ctx->page, mtd->writesize);
+			if(ret < 0) {
+				printf("FWU write err: off 0x%012llX ret %d\n",
+					ctx->flash_offset, ret);
+				goto err;
+			}
+
+			/* read back to force hardware CRC checks */
+			ret = mtd_read(mtd, ctx->flash_offset, ctx->page, mtd->writesize);
+			if(ret < 0) {
+				printf("FWU checkread err: off 0x%012llX ret %d\n",
+					ctx->flash_offset, ret);
+				goto err;
+			}
+
+			ctx->flash_offset += mtd->writesize;
+			ctx->page_sz = 0;
+		}
+	}
+
+err:
+	return ret;
 }
 
-static void replace_filename(char* path, const char *to)
+/* Extracts directory name with trailing '/' symbol or empty str if no dirs */
+static void extract_dirname(char *dir, const char* path)
 {
-	char* p = strrchr(path,'/');
+	const char* p = strrchr(path,'/');
 	if(p == NULL)
 		p = path;
 	else
 		p = p+1;
-	strncpy_s(p, to, MAXPATH - (p-path));
+	strncpy(dir, path, p-path);
+	dir[p-path]='\0';
 }
 
 static int do_fwupgrade(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
@@ -52,27 +115,35 @@ static int do_fwupgrade(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[
 		printf("FWU mtd device is not available\n");
 		return -1;
 	}
-	
-	struct fwu_tftp_ctx *ctx = &fwu_ctx;
-	u8 *page;
 
+	u8 *page;
 	page = malloc(mtd->writesize);
 	if(page == NULL) {
 		printf("FWU unable to allocate memory\n");
 		return -ENOMEM;
 	}
 
-	memset(ctx, 0, sizeof(struct fwu_tftp_ctx));
-	ctx->mtd = mtd;
-	ctx->page = page;
-	ctx->flash_offset = 0;
-	ctx->flash_end = mtd->size;
+	struct fwu_tftp_ctx ctx;
+	memset(&ctx, 0, sizeof(struct fwu_tftp_ctx));
+	ctx.mtd = mtd;
+	ctx.page = page;
+	ctx.flash_offset = 0;
+	ctx.flash_end = mtd->size;
 
 	struct NetTask task;
 	net_init_task_def(&task, TFTP);
 	task.u.tftp.data_cb = fwu_tftp_cb;
-	task.u.tftp.data_ctx = ctx;
-	replace_filename(task.bootfile, mtd->name);
+	task.u.tftp.data_ctx = &ctx;
+
+	char dir[MAXPATH];
+	extract_dirname(dir, task.bootfile);
+	if(0 == strcmp(mtd->name, "boot")) {
+		sprintf(task.bootfile, "%smboot.bin", dir);
+	} else if(0 == strcmp(mtd->name, "kernel")) {
+		sprintf(task.bootfile, "%suImage", dir);
+	} else {
+		sprintf(task.bootfile, "%smboot-%s.bin", dir, mtd->name);
+	}
 
 	ret = NetLoop(&task);
 	if (ret < 0) {
@@ -80,67 +151,13 @@ static int do_fwupgrade(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[
 		goto out;
 	}
 
+	printf("FWU complete: tftp_bytes %lu hex 0x%08lX flash_area 0x%012llX flash_bb %d\n",
+		task.out_filesize, task.out_filesize, ctx.flash_offset, ctx.bb);
+
 out:
 	free(page);
 	return 0;
 }
-
-//// 	printf("Starting incremental nand update\n");
-	//if (NULL==getenv("fwaddr"))
-	//{
-		//printf("fwaddr not set\n");
-		//return 0;
-	//}
-	//ulong addr = simple_strtoul(getenv("loadaddr"), NULL, 16);
-	//ulong fw_offset = simple_strtoul(getenv("fwaddr"), NULL, 16);
-	//nand_info_t *nand = &mtd_info[0];
-	//char* bootfile = strdup(getenv("bootfile"));
-	//char* tmp = malloc(128);
-	////Erase all the flash
-	//#ifdef CONFIG_FWU_ERASE
-	//nand_erase_options_t nand_erase_options;
-	//memset(&nand_erase_options, 0, sizeof(nand_erase_options));
-	//nand_erase_options.offset = fw_offset;
-//// 	nand_erase_options.spread =1;
-	//nand_erase_options.length = nand->size - fw_offset;	
- 	//printf("Cleaning [%d ==> %d / %d]\n", fw_offset, nand_erase_options.length, nand->size);
-	//if (nand_erase_opts(nand, &nand_erase_options))
-					//return 1;
-	//#endif
-	//int size;
-	//size_t pos = fw_offset;
-	//size_t end;
-	//size_t sz;
-	//int i=0;
-	//while (i<5)
-	//{
-		//sprintf(tmp, "%s.%d", bootfile, i++);
-//// 		printf("Loading: %s\n", tmp);
-		//setenv("bootfile",tmp);
-		//size = NetLoop(TFTP);
-		//if (size <= 0)
-		//{
-			//break;
-		//}
-		
-		//else if (size > 0)
-			//flush_cache(addr, size);
-		//size -= size % nand->erasesize;
-		//size += nand->erasesize;
-		//sz=size;
-		//end = pos + size;
-		//printf("Writing at 0x%x sz 0x%x\n", pos, sz);
-			//if (nand_write_skip_bad(nand, pos, &sz,
-					//(char*) addr))
-				//goto bailout;
-		//pos+=sz;
-		//if (pos % nand->erasesize)
-		 		//printf("Pos not aligned: 0x%x\n", pos);
-		//}
-		
-	//bailout:
-		//setenv("bootfile", bootfile);
-	//return 0;
 
 U_BOOT_CMD(
 	fwupgrade,	2,	0,	do_fwupgrade,
@@ -148,7 +165,9 @@ U_BOOT_CMD(
 	"\n"
 	"fwupgrade - upgrades current mtd device \n"
 	"fwupgrade MTD - upgrades mtd device MTD (can use a partition)\n"
-	" NOTE: TFTP file name is generated as follows:\n"
-	"       \"dirname($bootfile)/name_of(MTD)\"\n"
+	"  TFTP file names by partition:\n"
+	"  boot ..... dir($(bootfile))/mboot.bin\n" 
+	"  kernel ... dir($(bootfile))/uImage\n" 
+	"  NAME ..... dir($(bootfile))/mboot-NAME.bin\n"
 );
 

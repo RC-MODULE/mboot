15c77dad31a168ec1c90b54b46b2fd2c8cd772db
Sergey Mironov <ierton@gmail.com>
2012-05-30 14:43:46 +0400

    fwupgrade: dont't skip bad blocks but try to overwrite them
    Looks like SQUASHFS doesn't support plain bad blocks skipping - so it is better
    to try to overwrite them as is. Image can't be written to the device containing
    completely unusable blocks trough.

--- common/cmd_fwupgrade.c
+++ common/cmd_fwupgrade.c
@@ -1,13 +1,12 @@
 #include <common.h>
 #include <command.h>
 #include <errno.h>
-#include <malloc.h>
 #include <mtd.h>
 
 struct fwu_tftp_ctx {
 	struct mtd_info *mtd;
-	u8* page;
-	size_t page_sz;
+	u8* block;
+	size_t block_sz;
 	loff_t flash_offset;
 	loff_t flash_end;
 	int bb;
@@ -26,57 +25,68 @@ static int fwu_tftp_cb(uint32_t off, u8* buf, size_t len, int last, void* priv)
 
 	ret = 0;
 	while(len > 0) {
-		size_t len1 = MIN(mtd->writesize - ctx->page_sz, len);
-		memcpy(&ctx->page[ctx->page_sz], buf, len1);
+		size_t len1 = MIN(mtd->erasesize - ctx->block_sz, len);
+		memcpy(&ctx->block[ctx->block_sz], buf, len1);
 
-		ctx->page_sz += len1;
+		ctx->block_sz += len1;
 		buf += len1;
 		len -= len1;
 
-		if((ctx->page_sz == mtd->writesize) || last) {
-			if(ctx->flash_offset % mtd->erasesize == 0) {
-				ret = mtd_block_isbad(mtd, ctx->flash_offset);
-				if(ret < 0) {
-					printf("FWU bad block detection failure: off 0x%012llX\n",
-						ctx->flash_offset);
-					goto err;
-				}
-				else if (ret == 1) {
-					printf("FWU skipping bad block: off 0x%012llX\n",
-						ctx->flash_offset);
-					ctx->flash_offset += mtd->erasesize;
-					ctx->bb ++;
-					continue;
-				}
+		if((ctx->block_sz == mtd->erasesize) || last) {
 
-				printf("FWU rewriting block: off 0x%012llX\n",
+			int retry;
+			int bad = 0;
+
+			ret = mtd_block_isbad(mtd, ctx->flash_offset);
+			if(ret < 0) {
+				printf("FWU bad block detection failure: off 0x%012llX\n",
 					ctx->flash_offset);
+				goto err;
+			}
+			else if (ret == 1) {
+				printf("FWU bad block detected: off 0x%012llX\n",
+					ctx->flash_offset);
+				bad = 1;
+				ret = 0;
+			}
+
+			for(retry=0; (retry<5)&&(ctx->block_sz>0); retry++) {
+				printf("FWU rewriting block: off 0x%012llX%s%s\n",
+					ctx->flash_offset,
+					bad ? " (BAD)" : "",
+					retry == 0 ? "" : " (again)");
 
 				ret = mtd_erase1(mtd, ctx->flash_offset);
 				if(ret < 0) {
 					printf("FWU erase err: off 0x%012llX ret %d\n",
 						ctx->flash_offset, ret);
-					goto err;
+					continue;
 				}
+
+				ret = mtd_write(mtd, ctx->flash_offset, ctx->block, mtd->erasesize);
+				if(ret < 0) {
+					printf("FWU write err: off 0x%012llX ret %d\n",
+						ctx->flash_offset, ret);
+					continue;
+				}
+
+				/* read back to force hardware CRC checks */
+				ret = mtd_read(mtd, ctx->flash_offset, ctx->block, mtd->erasesize);
+				if(ret < 0) {
+					printf("FWU checkread err: off 0x%012llX ret %d\n",
+						ctx->flash_offset, ret);
+					continue;
+				}
+
+				ctx->flash_offset += ctx->block_sz;
+				ctx->block_sz = 0;
 			}
 
-			ret = mtd_write(mtd, ctx->flash_offset, ctx->page, mtd->writesize);
-			if(ret < 0) {
-				printf("FWU write err: off 0x%012llX ret %d\n",
+			if(ctx->block_sz > 0) {
+				printf("FWU failed to overwrite block 0x%012llX ret %d\n",
 					ctx->flash_offset, ret);
 				goto err;
 			}
-
-			/* read back to force hardware CRC checks */
-			ret = mtd_read(mtd, ctx->flash_offset, ctx->page, mtd->writesize);
-			if(ret < 0) {
-				printf("FWU checkread err: off 0x%012llX ret %d\n",
-					ctx->flash_offset, ret);
-				goto err;
-			}
-
-			ctx->flash_offset += mtd->writesize;
-			ctx->page_sz = 0;
 		}
 	}
 
@@ -98,10 +108,14 @@ static void extract_dirname(char *dir, const char* path)
 
 static int do_fwupgrade(struct cmd_ctx *cmdctx, int argc, char * const argv[])
 {
-	struct mtd_info *mtd;
+	struct mtd_info *mtd = NULL;
+	const char *filename = NULL;
 	int ret;
 
 	switch(argc) {
+		case 3:
+			filename = argv[2];
+			/* pass through */
 		case 2:
 			mtd = mtd_by_name(argv[1]);
 			break;
@@ -114,9 +128,12 @@ static int do_fwupgrade(struct cmd_ctx *cmdctx, int argc, char * const argv[])
 		return -1;
 	}
 
-	u8 *page;
-	page = malloc(mtd->writesize);
-	if(page == NULL) {
+	if(mtd->erasesize > CONFIG_CMD_FWUPGRADE_BUFFER_SIZE) {
+		printf("FWU internal buffer is smaller than size of eraseblock\n");
+		return -ENOMEM;
+	};
+	u8 *block = (u8*)CONFIG_CMD_FWUPGRADE_BUFFER_ADDR;
+	if(block == NULL) {
 		printf("FWU unable to allocate memory\n");
 		return -ENOMEM;
 	}
@@ -124,7 +141,7 @@ static int do_fwupgrade(struct cmd_ctx *cmdctx, int argc, char * const argv[])
 	struct fwu_tftp_ctx ctx;
 	memset(&ctx, 0, sizeof(struct fwu_tftp_ctx));
 	ctx.mtd = mtd;
-	ctx.page = page;
+	ctx.block = block;
 	ctx.flash_offset = 0;
 	ctx.flash_end = mtd->size;
 
@@ -135,12 +152,21 @@ static int do_fwupgrade(struct cmd_ctx *cmdctx, int argc, char * const argv[])
 
 	char dir[MAXPATH];
 	extract_dirname(dir, task.bootfile);
-	if(0 == strcmp(mtd->name, "boot")) {
-		sprintf(task.bootfile, "%smboot.img", dir);
-	} else if(0 == strcmp(mtd->name, "kernel")) {
-		sprintf(task.bootfile, "%suImage", dir);
+	if(filename) {
+		if(filename[0] == '/') {
+			strncpy_s(task.bootfile, filename, MAXPATH);
+		}
+		else {
+			sprintf(task.bootfile, "%s%s", dir, filename);
+		}
 	} else {
-		sprintf(task.bootfile, "%smboot-%s.bin", dir, mtd->name);
+		if(0 == strcmp(mtd->name, "boot")) {
+			sprintf(task.bootfile, "%smboot.img", dir);
+		} else if(0 == strcmp(mtd->name, "kernel")) {
+			sprintf(task.bootfile, "%suImage", dir);
+		} else {
+			sprintf(task.bootfile, "%smboot-%s.bin", dir, mtd->name);
+		}
 	}
 
 	ret = NetLoop(&task);
@@ -153,17 +179,17 @@ static int do_fwupgrade(struct cmd_ctx *cmdctx, int argc, char * const argv[])
 		task.out_filesize, task.out_filesize, ctx.flash_offset, ctx.bb);
 
 out:
-	free(page);
 	return 0;
 }
 
 U_BOOT_CMD(
 	fwupgrade,	2,	0,	do_fwupgrade,
 	"Firmware upgrade via TFTP",
-	"fwupgrade MTD - rewrites mtd device MTD using TFTP\n"
-	"  TFTP file names by partition:\n"
-	"  boot ..... dir($(bootfile))/mboot.img\n" 
-	"  kernel ... dir($(bootfile))/uImage\n" 
-	"  NAME ..... dir($(bootfile))/mboot-NAME.bin\n"
+	"fwupgrade MTD [FNAME] ...\n"
+	" - rewrites mtd device MTD using TFTP\n"
+	"     default TFTP file name is generated from MTD device name as follows:\n"
+	"     boot ..... dir($(bootfile))/mboot.img\n" 
+	"     kernel ... dir($(bootfile))/uImage\n" 
+	"     NAME ..... dir($(bootfile))/mboot-NAME.bin\n"
 );
 
